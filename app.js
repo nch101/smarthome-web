b64bd126c7357a9de36b760165e2d2969a88a685
nch101 <huynguyencong98@gmail.com>
2021-07-25 15:27:15 +0700

    add log4js

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError         =       require('http-errors');
+let express             =       require('express');
+let path                =       require('path');
+let cookieParser        =       require('cookie-parser');
+let log4js              =       require('log4js');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let log                 =       log4js.getLogger('app');
 
-var app = express();
+let indexRouter         =       require('./routes/index');
+let usersRouter         =       require('./routes/users');
+
+let app                 =       express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+app.use(log4js.connectLogger(log4js.getLogger('http'), { level: 'auto' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -24,18 +26,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
